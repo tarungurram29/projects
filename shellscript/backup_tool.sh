b9cf242cddca1f86b3f44fc1c85ecadafabf0917
Tarun <gurramtarun29@gmail.com>
2026-04-06 22:43:05 +0530

    Updated backup_tool.sh

--- shellscript/backup_tool.sh
+++ shellscript/backup_tool.sh
@@ -3,10 +3,15 @@
 # Author: Tarun Gurram
 # INFO: This script must run by root user for taking backups into tar files and deleting old tar files by retention days.
 ##########################################
+
+set -a
+source .env 
+set +a
+
 if [[ $UID -ne 0 ]]; then
+    echo "Run as root"
     exit 1
 fi
-
 
 LOG_PATH=/var/log/backup
 LOG_FILE="${LOG_PATH}"/backup.log
@@ -18,13 +23,29 @@ log(){
     echo "[$(date +"%Y-%m-%d_%H-%M-%S")] $1" >> "${LOG_FILE}" >/dev/null
 }
 
+send_mail(){
+    MESSAGE=$1
+    MAIL_MESSAGE=$2
+
+    echo "${MESSAGE}" | tee /dev/tty | mail \
+    -S smtp="smtp://smtp.gmail.com:587" \
+    -S smtp-auth=login \
+    -S smtp-auth-user="${MAIL_USER}" \
+    -S smtp-auth-password="${MAIL_PASS}" \
+    -S smtp-use-starttls \
+    -S ssl-verify=ignore \
+    -s "${MAIL_MESSAGE}" \
+    -c "${CC_EMAILS}" \
+    "${TO_EMAIL}"
+}
+
 taking_backup(){
     read -p "Enter source path" source
     read -p "Enter backup directory" backup
 
     if [[ -n "${source}" && -n "${backup}" ]]; then
         if [[ ! -d "${source}" ]]; then
-            echo "Source not found"
+            echo "Source directory not found"
             log "Source directory not found for taking the backup by user: $NAME"
             exit 1
         fi
@@ -38,10 +59,10 @@ taking_backup(){
         tar -czf "${backup}/${ARCHIVE_NAME}" -C "${source}" . 
 
         if [[ $? -eq 0 ]]; then
-            echo "Successfully taken the backup"
+            send_mail "Successfully taken the backup" "Successfull taken the backup by the user: ${NAME}"
             log "Successfully taken the backup by the user: ${NAME}"
         else
-            echo "failed"
+            send_mail "Failed to take the backup" "Failed to take the backup by the user: ${NAME}"
             log "Failed to take the backup by the user: ${NAME}"
             exit 1
         fi
@@ -58,11 +79,12 @@ deleting_backup(){
         exit 1
     else
         if [[ ! -d "${backup_path}" || "${retention}" -eq 0 ]]; then
-            echo "Backup folder doesnt exist AND enter retention day more than 0."
+            send_mail "Bakcup folder doesnt exist AND enter retention day more than 0" "Backup folder doesnt exist AND enter retention day more than 0. by user: $NAME"
             log "Backup folder doesnt exist AND enter retention day more than 0. by user: $NAME"
             exit 1
         else
-            find "${backup_path}" -type f -name "*.tar.gz" -mtime +"${retention}" -exec -f rm {} \;
+            find "${backup_path}" -type f -name "*.tar.gz" -mtime +"${retention}" -exec rm -f {} \;
+            send_mail "Successfull delete old backup files" "Successfully Deleted old backup files by the user: ${NAME}...Rentention days: $retention"
             log "Successfully Deleted old backup files by the user: ${NAME}...Rentention days: $retention"
         fi
     fi
